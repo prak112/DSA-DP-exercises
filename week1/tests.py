8894b3d1a9906c53701f67a2f3d363f52c326a1d
PrakGov <55034062+prak112@users.noreply.github.com>
2025-01-14 19:18:46 +0200

    exercises: Add unit tests for repeat.py

--- week1/tests.py
+++ week1/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from candies import count
+from repeat import find
 
 class TestCandiesCount(unittest.TestCase):
     def test_base_case(self):
@@ -16,6 +17,21 @@ class TestCandiesCount(unittest.TestCase):
         with self.assertRaises(TypeError):
             count('a', 'v', 10)
 
+
+class TestRepeatedPatternsCount(unittest.TestCase):
+    def test_base_case(self):
+        result = find('abcabcabc')
+        expected_output = 3
+        self.assertEqual(result, expected_output)
+
+    def test_edge_case(self):
+        result = find('pikpikapikapikpikapika')
+        expected_output = 11
+        self.assertEqual(result, expected_output)
+
+    def test_error_case(self):
+        with self.assertRaises(TypeError):
+            find(12312343)
 
 if __name__ == "__main__":
     unittest.main()
